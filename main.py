1eb52991e27e395361881778ce42f76471d0ca68
Marlene Pirker <mar.pirker@hotmail.com>
2021-11-04 19:16:41 +0100

    Lesson 16 - 5

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 webapp = Flask(__name__)
 
 @webapp.route("/")
 def index():
-    return render_template("index.html")
+    namen = "Hans"
+    return render_template("index.html", namen=namen, datum_today=datetime.now)
 
 @webapp.route("/about")
 def about():
@@ -12,6 +14,7 @@ def about():
 
 @webapp.route("/projects")
 def project():
+    projects= ["Flask Projects", "Boogle", "Fakebook"]
     return render_template("Project.html")
 
 if __name__ == "__main__":
