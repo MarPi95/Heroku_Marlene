e11360ecd773bbbbbced1b136501411f88c72a71
Marlene Pirker <mar.pirker@hotmail.com>
2021-11-08 19:27:25 +0100

    Lesson 17 - 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from datetime import datetime
 
 webapp = Flask(__name__)
@@ -9,7 +9,8 @@ authors = []
 def index():
     namen = "Hans"
     print(request.args)
-    return render_template("index.html", namen=namen, datum_today=datetime.now(), p=request.args.get("p"))
+    cookie_value = request.cookies.get("cookie_value")
+    return render_template("index.html", namen=namen, datum_today=datetime.now(), cookie_value =cookie_value)
 
 @webapp.route("/about", methods=["GET", "POST"])
 def about():
@@ -34,6 +35,29 @@ def contact():
 def project():
     projects= ["Flask Projects", "Boogle", "Fakebook"]
     return render_template("Project.html", projects = projects)
+
+@webapp.route("/setcookie", methods=["GET", "POST"])
+def setcookie():
+    if request.method == "POST":
+        cookie_value = request.form.get("cookie")
+        response = make_response(render_template("setcookie.html", cookie_value=cookie_value))
+        response.set_cookie("cookie_value", cookie_value)
+    else:
+        if request.args.get("action") == "delete":
+            response = make_response(render_template("setcookie.html", cookie_value=None))
+            response.set_cookie("cookie_value", expires=0)
+        else:
+            cookie_value = request.cookies.get("cookie_value")
+            response = make_response(render_template("setcookie.html", cookie_value=cookie_value))
+
+    return response
+
+@webapp.route("/del_cookie")
+def del_cookie():
+    response = make_response(render_template("setcookie.html"))
+    response.set_cookie("cookie_value", expires=0)
+
+    return response
 
 if __name__ == "__main__":
     webapp.run()
